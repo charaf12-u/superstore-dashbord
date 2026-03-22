80d150534f9504314e1d9c1a7639164ef081e257
charaf <charafsbai01@gmail.com>
2026-03-22 21:41:53 +0000

    Update database connection logic and add images

--- connectionBD/db_connection.py
+++ connectionBD/db_connection.py
@@ -2,14 +2,17 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
+# --> tahmil les variable dans .env
 load_dotenv()
 
+# --> enregistrer le contenu dans un variable
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "superstore_db")
 
+# --> function de connection
 def get_engine():
     try:
         engine = create_engine(
