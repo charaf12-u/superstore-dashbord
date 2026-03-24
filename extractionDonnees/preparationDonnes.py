c34121c5028ac4ea632bbeefea977c11c02dd611
SOUBI Charaf <155108346+charaf12-u@users.noreply.github.com>
2026-03-24 11:31:12 +0100

    Change JOINs to FULL OUTER JOINs in SQL query

--- extractionDonnees/preparationDonnes.py
+++ extractionDonnees/preparationDonnes.py
@@ -10,15 +10,15 @@ def preparation(engine):
                 r.region_name, c.category_name, d.order_date,
                 p.product_name, cl.customer_name
             FROM ventes v
-            JOIN commande co ON v.order_id = co.order_id
-            JOIN date_temps d ON co.date_id = d.date_id
-            JOIN produit p ON v.product_id = p.product_id
-            JOIN sous_categorie sc ON p.sub_category_id = sc.sub_category_id
-            JOIN categorie c ON sc.category_id = c.category_id
-            JOIN client cl ON co.customer_id = cl.customer_id
-            JOIN localisation l ON co.postal_code = l.postal_code
-            JOIN etat e ON l.state_id = e.state_id
-            JOIN region r ON e.region_id = r.region_id
+            FULL OUTER JOIN commande co ON v.order_id = co.order_id
+            FULL OUTER JOIN date_temps d ON co.date_id = d.date_id
+            FULL OUTER JOIN produit p ON v.product_id = p.product_id
+            FULL OUTER JOIN sous_categorie sc ON p.sub_category_id = sc.sub_category_id
+            FULL OUTER JOIN categorie c ON sc.category_id = c.category_id
+            FULL OUTER JOIN client cl ON co.customer_id = cl.customer_id
+            FULL OUTER JOIN localisation l ON co.postal_code = l.postal_code
+            FULL OUTER JOIN etat e ON l.state_id = e.state_id
+            FULL OUTER JOIN region r ON e.region_id = r.region_id
         """
 
         # --> charger data
